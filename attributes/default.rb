f6a31e85b518badd5cc83f4759c31eb889e20a78
Paweł Pacana <pawel.pacana@gmail.com>
2013-03-07 17:20:41 +0100

    Initial version.

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['nullmailer']['remotes']   = []
+default['nullmailer']['mailname']  = nil
+default['nullmailer']['adminaddr'] = nil
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'nullmailer'
+maintainer       'Arkency'
+maintainer_email 'pawel.pacana@gmail.com'
+license          'MIT'
+description      'Installs/Configures nullmailer'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+supports         'debian'
+supports         'ubuntu'
